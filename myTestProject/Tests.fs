d40a5318c18498938a1b90e02e55e633fed9b117
jamesa <james.aslett@directlife.co.uk>
2021-07-20 08:58:51 +0100

    Add project files.

---
+++ myTestProject/Tests.fs
@@ -0,0 +1,16 @@
+﻿module Tests
+
+open NUnit.Framework
+open canopy.classic
+
+[<TestFixture>]
+type TestCases() =
+
+    [<TestCase(TestName="My First Test", Category="Test Cases")>]
+        member _.My_First_Test() =
+            start chrome
+            url "http://lefthandedgoat.github.io/canopy/testpages/"
+            clear "#testfield1"
+            "#testfield1" << "Test field 1 input"
+            click "#button"
+            click "#hyperlink"
